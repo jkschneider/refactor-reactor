707f85dc2266bf5a29c0ef1a945746afff2f1eb4
Jon Schneider <jkschneider@gmail.com>
2017-04-21 22:51:33 -0500

    Use the plugins block to install io.spring.rewrite

--- example/src/main/java/com.example/Example.java
+++ example/src/main/java/com.example/Example.java
@@ -7,8 +7,8 @@ public class Example {
         Mono.just(1).then(Mono.just(2));
         Mono.just(1).then(a -> Mono.just(a + 2));
 
-        Flux.just(1).onErrorResume(t -> Mono.just(2));
-        Flux.just(1).onErrorResume(IllegalArgumentException.class, t -> Mono.just(2));
-        Flux.just(1).onErrorResume(e -> true, t -> Mono.just(2));
+        Flux.just(1).switchOnError(Mono.just(2));
+        Flux.just(1).switchOnError(IllegalArgumentException.class, Mono.just(2));
+        Flux.just(1).switchOnError(e -> true, Mono.just(2));
     }
 }
